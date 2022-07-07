7d18fb38cf377d38148e578757e99118131eb8c3
simon <1354527247@qq.com>
2022-07-07 09:14:48 +0800

    Homework

--- day28/Home1.py
+++ day28/Home1.py
@@ -1,10 +1,13 @@
-# 小彭的乱码
-import logging
+# # 小彭的乱码
+# import logging
+#
+# logging.basicConfig(level=logging.DEBUG, filename='log.txt', filemode='a',encoding='utf8',
+#                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+# logging.debug('debug')
+# logging.info('info')
+# logging.warning('warming')
+# logging.error('error')
+# logging.critical('critical')
 
-logging.basicConfig(level=logging.DEBUG, filename='log.txt', filemode='a',encoding='utf8',
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
-logging.debug('debug')
-logging.info('info')
-logging.warning('warming')
-logging.error('error')
-logging.critical('critical')
+x = ['asd', 'asd']
+print(len(x))
--- day28/Home2.py
+++ day28/Home2.py
@@ -1,15 +1,21 @@
-# 小彭的乱码
-def orange(self):
-    print("orange's color is orange")
+# # 小彭的乱码
+# def orange(self):
+#     print("orange's color is orange")
+#
+#
+# class Fruit:
+#     def output_fruit(self):
+#         print("Fruit have apple and orange")
+#
+#
+# Food = type('Food', (Fruit,), {'apple': 'tasty', 'orange': orange})
+# food = Food()
+# food.orange()
+# food.output_fruit()
+# print(food.apple)
 
-
-class Fruit:
-    def output_fruit(self):
-        print("Fruit have apple and orange")
-
-
-Food = type('Food', (Fruit,), {'apple': 'tasty', 'orange': orange})
-food = Food()
-food.orange()
-food.output_fruit()
-print(food.apple)
+import matplotlib.pyplot as plt
+# 传入x和y, 通过plot画图
+plt.plot([1, 0, 9], [4, 5, 6])
+# 在执行程序的时候展示图形
+plt.show()
